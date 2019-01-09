c4ff0c216026a5123a29cae0fd044286467ff62e
Dan Martz <dansdevelopments@gmail.com>
2019-01-09 11:01:51 -0600

    Hash Tables Assignment Problem 6 complete

---
+++ data/customers.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    name: "Harrington Hopper",
+    phone: "(838) 595-2361",
+    address: "130 Keap Street, Ladera, Iowa, 2018"
+  },
+  {
+    name: "Rosie Farmer",
+    phone: "(947) 532-2650",
+    address: "803 Ashford Street, Riceville, New Mexico, 5116"
+  },
+  {
+    name: "Dillon Waters",
+    phone: "(907) 414-2068",
+    address: "499 Jaffray Street, Hampstead, Arkansas, 2426"
+  },
+  {
+    name: "Jodi Brown",
+    phone: "(995) 402-2219",
+    address: "716 Eldert Lane, Nicut, Utah, 1796"
+  },
+  {
+    name: "Tricia Donovan",
+    phone: "(964) 415-3371",
+    address: "479 Karweg Place, Cornucopia, South Dakota, 6428"
+  },
+  {
+    name: "Kaufman Estrada",
+    phone: "(967) 402-2055",
+    address: "457 Hubbard Place, Comptche, Ohio, 6223"
+  }
+];
---
+++ hashTable-customers.js
@@ -0,0 +1,2 @@
+import HashTable from "./modules/HashTable";
+import customerData from "./data/customers";
